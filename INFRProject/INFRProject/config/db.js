7a6f09e543741a99dfdc13e39809179940829a20
Daniel Okoro <daniel.ookoro@gmail.com>
2024-11-12 21:15:04 -0500

    mongodb connection timeout

--- INFRProject/INFRProject/config/db.js
+++ INFRProject/INFRProject/config/db.js
@@ -6,7 +6,9 @@ const MONGODB_URI = process.env.MONGODB_URI_DANIEL
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase the timeout to 30 seconds
+    socketTimeoutMS: 45000 // Optional: timeout for socket operations
 })
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log("Error connecting to MongoDB:", err));
